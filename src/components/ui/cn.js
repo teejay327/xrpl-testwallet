950218a908e49b4d72dafc928d89e0d9421642f3
teejay327 <mrenergy@hotmail.com>
2026-02-20 22:33:38 +1000

    Modified cn.js to make it merge conflicts

--- src/components/ui/cn.js
+++ src/components/ui/cn.js
@@ -1,5 +1,8 @@
-const cn = (...classes) => {
-  return classes.filter(Boolean).join(" ");
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+const cn = (...inputs) => {
+  return twMerge(clsx(inputs));
 }
 
 export default cn;
