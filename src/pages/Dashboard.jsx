074b863d654bd8764ab2550062793dfc3d9fa0bc
teejay327 <mrenergy@hotmail.com>
2026-03-27 21:43:53 +1000

    Changed time

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -102,7 +102,7 @@ const Dashboard = () => {
 
       const timer = setInterval(() => {
         refresh();
-      },15000);
+      },30000);
       return () => clearInterval(timer);
 
       // eslint-disable-next-line react-hooks/exhaustive-dep
