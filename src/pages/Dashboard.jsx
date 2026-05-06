bf38aaa58dcf7c4f18d3311d1f7bca9192b3f910
teejay327 <mrenergy@hotmail.com>
2026-05-06 22:55:24 +1000

    Changed fade time for transasctions

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -142,7 +142,7 @@ const Dashboard = () => {
         setTxMessage("");
         setTxHash("");
         setIsFading(false);
-      },3000);
+      },4000);
 
       return() => {
         clearTimeout(fadeTimer);
