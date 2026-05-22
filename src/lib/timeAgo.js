836c788becb4722e941e962175398d930150ea92
teejay327 <mrenergy@hotmail.com>
2026-05-22 23:50:45 +1000

    Added timeAgo.js to add relative timestamps

---
+++ src/lib/timeAgo.js
@@ -0,0 +1,20 @@
+const timeAgo = (timestamp) => {
+  if (!timestamp) return "";
+
+  const seconds = Math.floor(
+    (Date.now() -  new Date(timestamp)) / 1000
+  );
+
+  if (seconds < 60) return `${seconds}s ago`;
+
+  const minutes = Math.floor(seconds / 60);
+  if (minutes < 60) return `${minutes}m ago`;
+
+  const hours = Math.floor(minutes / 60);
+  if (hours < 24) return `${hours}h ago`;
+
+  const days = Math.floor(hours / 24);
+   return `${days}d ago`;
+}
+
+export default timeAgo;
