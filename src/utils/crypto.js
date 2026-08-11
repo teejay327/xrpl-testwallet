567a2e37c37311e1b8b1a550452977659f7a9c5e
teejay327 <mrenergy@hotmail.com>
2026-08-11 16:36:44 +1000

    Building encryption key in crypto.js

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -15,8 +15,37 @@ const encryptSeed = async(seed, key) => {
   const iv = crypto.getRandomValues(new Uint8Array(12));
 }
 
+const deriveEncryptionKey = async(password) => {
+  const encoder = new TextEncoder();
+
+  const keyMaterial =await crypto.subtle.importKey(
+    "raw",
+    encoder.encode(password),
+    "PBKDF2",
+    false,
+    ["deriveKey"]
+  );
+
+  return crypto.subtle.deriveKey(
+    {
+      name: "PBKDF2",
+      salt,
+      iterations: 250_000,
+      hash: "SHA-256"
+    },
+    keyMaterial,
+    {
+      name: "AES-GCM",
+      length: 256
+    },
+    false,
+    ["encrypt", "decrypt"]
+  );
+};
+
 export {
   bytesToBase64,
   base64ToBytes, 
-  encryptSeed
+  encryptSeed,
+  deriveEncryptionKey
 };
