2b2cd39eb2db815948908087fed0bad8f2fa081d
teejay327 <mrenergy@hotmail.com>
2026-08-17 21:50:05 +1000

    Changed Dashboard text

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -63,7 +63,7 @@ const Dashboard = () => {
 
     if (!amount.trim()) {
       setTxType("error");
-      setTxMessage("Please enter an amount");
+      setTxMessage("Please enter amount");
       setTxHash("");
       return;
     }
